9136172906ffe7d4dec624deb2bbe3587f7ea387
樊亚康 <2944312521@qq.com>
2023-05-22 19:22:05 +0800

    第四次提交

---
+++ js/kuwo.js
@@ -0,0 +1,15 @@
+var e1 = document.getElementsByClassName('aa')[0];
+var e2 = document.getElementsByClassName('a2')[0];
+console.log(e1);
+console.log(e2);
+
+e1.onclick = function () {
+    e1.className = 'aaa';
+    e2.className = 'a3'
+}
+
+var e3 = document.getElementById('dz');
+console.log(e3);
+e3.onclick = function () {
+
+}
